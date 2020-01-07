a80efcfb8dc28d7e03c8f3769e7de7a2a57310c4
Matheus Pedroni <matheusr@ciandt.com>
2020-01-07 14:03:52 -0300

    first commit

---
+++ precache-manifest.a1bade99cee4c600708e14aa717499ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "73e796f74b4b941bc9b5",
+    "url": "/css/app.ef153b65.css"
+  },
+  {
+    "revision": "73e796f74b4b941bc9b5",
+    "url": "/js/app.08e9a7f8.js"
+  },
+  {
+    "revision": "e7c2294a70c75cf8f97b",
+    "url": "/js/chunk-vendors.98b5c160.js"
+  },
+  {
+    "revision": "f9e15284367ca515c7f1cc146e5d831e",
+    "url": "/img/github.f9e15284.png"
+  },
+  {
+    "revision": "7f163d2b3f92f30654b6f2fb6b200682",
+    "url": "/img/profile.7f163d2b.jpg"
+  },
+  {
+    "revision": "76102893d11c451fef846b6a82fa4dd8",
+    "url": "/img/facebook.76102893.png"
+  },
+  {
+    "revision": "1911c85b868e0a0d2f974170a3d3d999",
+    "url": "/img/instagram.1911c85b.png"
+  },
+  {
+    "revision": "d1d54f80d17b5cd80f1cd1482d7fa3db",
+    "url": "/img/linkedin.d1d54f80.png"
+  },
+  {
+    "revision": "de1b904cad3f25d874b2aa1070a3bc26",
+    "url": "/img/twitter.de1b904c.png"
+  },
+  {
+    "revision": "44b07fb90d2859b189751318f6d43733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
